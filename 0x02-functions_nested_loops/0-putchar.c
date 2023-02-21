0f0672f86e4e40053a4db591e3ef54c6a11244b0
Josh Kisb <joshuakisb@gmail.com>
2023-02-21 05:54:52 -0800

    print using putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print using putchar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');	
+	putchar('p');	
+	putchar('u');	
+	putchar('t');	
+	putchar('c');	
+	putchar('h');	
+	putchar('a');	
+	putchar('r');	
+	putchar('\n');	
+	
+	return (0);
+}
