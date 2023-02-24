d1e125da9b2e2ac3ff4ea65ac0d483d57a192881
Josh Kisb <joshuakisb@gmail.com>
2023-02-23 18:55:27 -0800

    more num

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print the alphabet
+ *
+ * Description: print the ascii alphabet using _putchar
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int num = 0, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (num = 0; num < 15; num++)
+		{
+			if (num > 9)
+				_putchar('0' + (num/10));
+			_putchar('0' + (num%10));
+		}
+	}
+	_putchar('\n');
+}
