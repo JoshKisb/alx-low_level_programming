8ef9378bdc7d7eae7beda2186ad1dbf27e54cd12
Josh Kisb <joshuakisb@gmail.com>
2023-02-23 18:35:23 -0800

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - add 2 numbers
+ * @num1: The first number
+ * @num2: The second number
+ *
+ * Return: int, The mul of the 2 numbers
+ */
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
