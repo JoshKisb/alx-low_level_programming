eb1b4276e5ca7b79dea64efaa7b0081e41788c27
Josh Kisb <joshuakisb@gmail.com>
2023-02-23 18:40:06 -0800

    ptint numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the alphabet
+ *
+ * Description: print the ascii alphabet using _putchar
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int letter = '0';
+
+	while (letter < '9')
+		_putchar(letter++);
+
+	_putchar('\n');
+}
