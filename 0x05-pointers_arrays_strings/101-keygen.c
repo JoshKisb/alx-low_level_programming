46197414b08a2c0bfbf9dbd81b71840e2c7a1241
Josh Kisb <joshuakisb@gmail.com>
2023-02-27 17:39:54 -0800

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define CHECKSUM_TARGET 2772
 
 /**
  * main - generate valid password
@@ -7,8 +11,28 @@
  */
 int main(void)
 {
-    char *s = "aaaaaaaaaaaaaaaaaaaaaaaa111111114";
+	srand(time(NULL));
+	char str[100];
+	int checksum = 0;
 
-    printf("%s", s);
-    return (0);
+	do {
+		/* Generate a random string */
+		for (int i = 0; i < 99; i++)
+		{
+			/* Generate a random ASCII character between 32 and 126 */
+			str[i] = rand() % 94 + 32;
+		}
+		str[99] = '\0';
+
+		/* Calculate the checksum */
+		checksum = 0;
+		for (int i = 0; str[i] != '\0'; i++)
+		{
+			checksum += (int) str[i];
+		}
+
+	} while (checksum != CHECKSUM_TARGET); /* generate strings til checksum got */
+
+	printf("%s", str);
+	return (0);
 }
