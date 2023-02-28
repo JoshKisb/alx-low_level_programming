3fcd0d645a5b4e40b601f752ca7f9aed012bcf32
Josh Kisb <joshuakisb@gmail.com>
2023-02-27 17:21:56 -0800

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - generate valid password
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "aaaaaaaaaaaaaaaaaaaaaaaa111111114";
+
+    printf("%s", s);
+    return (0);
+}
