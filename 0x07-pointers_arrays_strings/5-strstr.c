e04eed56428d18b2de679d0a4ebfe5d01a9bea60
Josh Kisb <joshuakisb@gmail.com>
2023-03-06 13:25:24 -0800

    strstr no check

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - search for substr in string
+ * @haystack: The string to search
+ * @needle: The substring to search for
+ *
+ * Return: the first match addr or NULL for no match
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, match;
+
+	for (; *haystack != '\0'; haystack++)
+	{
+		match = 1;
+
+		for (i = 0; needle[i] != '\0' && haystack[i] != '\0'; i++)
+		{
+			if (haystack[i] != accept[i])
+			{
+				match = 0;
+				break;
+			}
+		}
+
+		if (match)
+			return (haystack);
+	}
+
+	return (NULL);
+}
