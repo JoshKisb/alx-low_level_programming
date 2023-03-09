38f85a1ba5d5515ac9a2986505a0a94d064b13ef
Josh Kisb <joshuakisb@gmail.com>
2023-03-08 17:42:59 -0800

    ah sqlt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+/**
+ * guess - try guess the sqrt
+ * @a:high
+ * @n: The number
+ *
+ * Return: int
+ */
+int sqrt_search(int x, int n)
+{
+	if (x * x == n)
+		return (x);
+	if (x * x > n)
+		return (-1);
+	return (sqrt_search(x + 1, n));
+}
+
+/**
+ * _sqrt_recursion - calc sqrt of a number
+ * @n: The number
+ *
+ * Return: sqrt of number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	return (sqrt_search(1, n));
+}
