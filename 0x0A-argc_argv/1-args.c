6bf33b7d744b595c93510ad5209a58fa91d7aa8f
Josh Kisb <joshuakisb@gmail.com>
2023-03-10 10:47:13 -0800

    number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: The array of args
+ *
+ * Description: print the number of args
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
