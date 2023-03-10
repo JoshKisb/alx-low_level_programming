eddf4be664c1c40563a84adf221355cb42502c07
Josh Kisb <joshuakisb@gmail.com>
2023-03-10 10:58:57 -0800

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: The array of args
+ *
+ * Description: print the number of args
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+		printf("Error\n");
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
