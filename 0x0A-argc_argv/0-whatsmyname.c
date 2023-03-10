f7beb452dbff44d3929a6ca954df8a9c14186caa
Josh Kisb <joshuakisb@gmail.com>
2023-03-10 10:44:07 -0800

    whays ma name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: The array of args
+ *
+ * Description: print the program name
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
